c39e45018440cc25e77f884f16a404163f84b988
waynedane <waynedane@outlook.com>
2018-08-23 00:17:36 +0800

    Update model.py

--- model.py
+++ model.py
@@ -10,11 +10,12 @@ class Resblock(Block):
         self.dropout = dropout
         self.resblock = nn.Sequential()
         with self.resblock.name_scope():
-            self.resblock.add(nn.LayerNorm)
-            self.resblock.add(nn.Dense(2*self.model_dim, activation="relu"))
+            self.resblock.add(nn.LayerNorm())
+            self.resblock.add(nn.Dense(2*self.model_dim,in_units= self.model_dim,activation="relu"))
             self.resblock.add(nn.Dropout(self.dropout))
-            self.resblock.add(nn.Dense(self.model_dim, activation="relu"))
+            self.resblock.add(nn.Dense(self.model_dim,in_units = 2*self.model_dim))
             self.resblock.add(nn.Dropout(self.dropout))
+            
     def forward(self, x):
         output = self.resblock(x)
         return output+x
