add5560cc9a9f75eb749e5b87874395d96c0bf64
waynedane <waynedane@outlook.com>
2018-08-31 12:27:25 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
+from mxnet import nd
 def get_length(x):
     x = (x!=0)
     length = x.sum(1)
     return length
+
+def return_mask(key,query):
+    K = (key!=0).expand_dims(-1)
+    Q =(query!=0).expand_dims(-1)
+    mask = nd.batch_dot(Q, K.tranpose(2,1))
+    return mask
