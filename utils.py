0e6972adf8e7eb55d6de93b441cf0bcb93b90773
waynedane <waynedane@outlook.com>
2018-09-01 09:03:00 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def get_length(x):
     return length
 
 def return_mask(key,query):
-    K = (key!=0).expand_dims(-1)
+    K = (key!=0).expand_dims(1)
     Q =(query!=0).expand_dims(-1)
-    mask = nd.batch_dot(Q, nd.tranpose(K, axes=(0,2,1)))
+    mask = nd.batch_dot(Q, K)
     return mask
