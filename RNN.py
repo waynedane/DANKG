fe6d8e2b9667653f7fd07f37003a6a5beb18f34d
waynedane <waynedane@outlook.com>
2018-09-02 10:30:03 +0800

    Update RNN.py

--- RNN.py
+++ RNN.py
@@ -22,8 +22,10 @@ class GRU(nn.Block):
             outputs, state = self.rnn(x,hidden)
         outputs = nd.transpose(outputs,(1,0,2))
         outputs = nd.SequenceMask(outputs, sequence_length=length,use_sequence_length=True, value=0)
-        
-        return nd.transpose(outputs,(1,0,2))
+        outputs = nd.transpose(outputs,(1,0,2))
+        hidden =[output[i-1] for (output,i) in zip(outputs,length)]
+        hidden = nd.stack(*hidden).squeeze()
+        return outputs, hidden
     
     
 class LSTM(Block):
@@ -39,11 +41,14 @@ class LSTM(Block):
                  input_size= num_inputs, i2h_weight_initializer ='Orthogonal', h2h_weight_initializer= 'Orthogonal'
                            )
         
-   def forward(self, x,length):
+   def forward(self, x,length, (hidden,cell) =None):
       
-        outputs, _ = self.rnn(x) #outputs:[batch, seq_length, 2*num_hiddens]
-        outputs = outputs.transpose(1,0,2)
-        outputs = nd.SequenceMask(outputs, sequence_length=lenghth,use_sequence_length=True, value=0)
-        hidden = nd.stack([outputs[:,i,:] for i in length])
-        
-        return outputs.transpose(1,0,2), hidden
+        outputs = self.rnn(x) #outputs:[batch, seq_length, 2*num_hiddens]
+        if hidden is not None:
+            outputs, state = self.rnn(x,(hidden, cell))
+        outputs = nd.transpose(outputs,(1,0,2))
+        outputs = nd.SequenceMask(outputs, sequence_length=length,use_sequence_length=True, value=0)
+        outputs = nd.transpose(outputs,(1,0,2))
+        hidden =[output[i-1] for (output,i) in zip(outputs,length)]
+        hidden = nd.stack(*hidden).squeeze()
+        return outputs, hidden
