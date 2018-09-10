0a4988d30921c022b68f29416bddff8094cc22d4
waynedane <waynedane@outlook.com>
2018-09-10 20:30:14 +0800

    Update customlayer.py
    Fix bugs

--- customlayer.py
+++ customlayer.py
@@ -1,13 +1,14 @@
 from mxnet.gluon import nn
+import mxnet as mx
 
 class ScaleShift(nn.Block):
-    def __init__(self, vocab_size):
-        super(ScaleShift, self).__init__()
+    def __init__(self, vocab_size, **kwargs):
+        super(ScaleShift, self).__init__(**kwargs)
         self.vocab_size = vocab_size
         self.w =self.params.get('weight', shape=(1,self.vocab_size))
         self.b = self.params.get('bias', shape=(1,self.vocab_size),init=mx.init.Zero())
         #self.w = mx.gluon.Parameter('weight', shape=(1, self.vocab_size), init=mx.init.Xavier())
         #self.b = mx.gluon.Parameter('bias', shape=(1, self.vocab_size), init=mx.init.Zero())
     def forward(self,x):
-        out =self.w.data()*x+self.b.data()
-        return out
+        output =self.w.data()*x+self.b.data()
+        return output
