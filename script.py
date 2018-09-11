ecd3e8b6a58fda1ee61d633be6be210b96b85170
waynedane <waynedane@outlook.com>
2018-09-11 10:31:02 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,21 @@
 import pickle
+import numpy as np
 def getdata(path):
     with open(path,'rb') as f:
         data = pickle.load(f)
     return data
+def process(list_of_string):
+    a = ' '.join(list_of_string)
+    a= a.replace('\\', '')
+    a = a.replace('/', '')
+    return a.split(' ')
+def to_array(instance, dict_):
+    content = instace[0]
+    keyphrases = instance[1]
+    title = content[0]
+    abstract = content[1]
+    title = process(title)
+    abstract = process(abstract)
+    title = np.array([dict_[token] for token in title])
+    abstract = np.array([dict_[token] for token in abstract])
+    
