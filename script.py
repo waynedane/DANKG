51360d9e67700e75379f816f7186949777f6a296
waynedane <waynedane@outlook.com>
2018-09-11 12:31:49 +0800

    Update script.py

--- script.py
+++ script.py
@@ -4,11 +4,18 @@ def getdata(path):
     with open(path,'rb') as f:
         data = pickle.load(f)
     return data
+
 def process(list_of_string):
     a = ' '.join(list_of_string)
     a= a.replace('\\', '')
     a = a.replace('/', '')
     return a.split(' ')
+
+def padpad(array, max_length):
+    length = len(array)
+    ndarray = np.pad(array, (0,max_length-length), 'constant', constant_values=0)
+    return ndarray
+
 def to_array(instance, dict_):
     content = instace[0]
     keyphrases = instance[1]
@@ -17,5 +24,18 @@ def to_array(instance, dict_):
     title = process(title)
     abstract = process(abstract)
     title = np.array([dict_[token] for token in title])
+    title = padpad(title, 30)
     abstract = np.array([dict_[token] for token in abstract])
+    abstract = padpad(abstract,400)
+    content = np.concatenate((title,abstract))
+    reps = len(keyprhases)
+    content = np.tile(content,(reps,1))
     
+    for i in range(reps):
+        keyphrase = keyphrases[i]
+        keyphrase = np.array([dict_[token] for token in keyphrase])
+        keyphrase = padpad(keyhprase, 6)
+        
+    keyphrases = np.array(keyphrases)
+    output = np.concatenate((content,kyphrases), -1)
+    return output
