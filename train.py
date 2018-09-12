e7d8e3fade8b49264ef6b40d114948ac5830f13b
waynedane <waynedane@outlook.com>
2018-09-12 18:29:16 +0800

    Update train.py

--- train.py
+++ train.py
@@ -8,7 +8,8 @@ Embedding_Dim, Vocab_Size, Extended_Size, Model_Dim, Head, drop_prob, dropout, t
 Constant.Embedding_Dim, Constant.Vocab_Size, Constant.Extended_Size,\ 
 Constant.Model_Dim, Constant.Head, Constant.drop_prob,\
 Constant.dropout, Constant.tearcher_forcing, Constant.learning_rate
-
+#定义gpu
+ctx = mxnet.gpu()
 #初始化模型
 net = model.seq2seq(Embedding_Dim, Head, Model_Dim, drop_prob, dropout, Vocab_Size, Extended_Size, tearcher_forcing)
 net.initialize(ctx=mx.gpu())
@@ -16,7 +17,7 @@ net.initialize(ctx=mx.gpu())
 
 dataset = myDataLodaer('/home/dwy/DKGMA_data', 'train')
 for index, instance in enumerate(dataset):
-    t,a,k = instance.as_in_context(mxnet.gpu())
+    t,a,k = instance.as_in_context(ctx)
     title = utils.get_batch(t)
     abstract = utils.get_batch(a)
     
